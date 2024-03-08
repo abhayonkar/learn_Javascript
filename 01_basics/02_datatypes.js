4255660a5f76309039cf22003aa7e3fe5707bd90
abhayonkar <abhayronkar@gmail.com>
2024-03-08 10:06:28 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version // this is not a import command 
+
+// alert("hello") // use node.js not browser, this can be run only on console of a browser
+
+// console.log(3+4); console.log("abhay") // wrong practice, it works  but decreases readability
+
+let name = "abhay"
+let age = 22
+let isLoggedIn = false
+
+// number => can store upto 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object =>
+
+console.log(typeof "abhay"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
