0b98233ab8b828f5fc5bf875fb20c260cfed0550
abhayonkar <abhayronkar@gmail.com>
2024-05-17 01:03:26 +0530

    json-object

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,19 @@
+const course = {
+    course_name: "jss",
+    price: "767",
+    courseid:"js01"
+}
+
+const {course_name} = course
+console.log(course_name)
+
+const {courseid: id} = course
+
+console.log(id)
+
+{ // sample json data
+    "name": "abhay",
+    "lname": "onkar",
+    "degree": "b.e"
+}
+
