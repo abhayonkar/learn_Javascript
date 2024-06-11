23fce8d8caab35347b9cbf60accbda2ca7bf6955
abhayonkar <abhayronkar@gmail.com>
2024-06-11 10:43:43 +0530

    events-dom

---
+++ 06_DOM!/data.js
@@ -0,0 +1,5 @@
+let p_tag_ref = document.getElementsByTagName('abc')
+
+console.log(p_tag_ref)
+
+p_tag_ref.innerHTML = "GM"
---
+++ dom03/data.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    let p_tag_ref = document.getElementsByTagName('button')[0]
+
+console.log(p_tag_ref)
+
+p_tag_ref.style.backgroundColor = 'blue'
+}
