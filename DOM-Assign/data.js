2002cfe29cf5a5887c77c3d7aada222952ca73ca
abhayonkar <abhayronkar@gmail.com>
2024-06-12 00:26:56 +0530

    assignment

---
+++ DOM-Assign/data.js
@@ -0,0 +1,30 @@
+function mouseOver() {
+  let btn = document.getElementsByTagName('button')[0]
+    btn.style.backgroundColor = 'blue'
+}
+
+
+function mouseOut() {
+  let btn1 = document.getElementsByTagName('button')[1]
+    btn1.style.backgroundColor = 'red'
+}
+
+function clickEvent(){
+    let btn2 = document.getElementsByTagName('button')[2]
+    btn2.style.backgroundColor = 'blue'
+}
+
+function dblClick(){
+    let btn3 = document.getElementsByTagName('button')[3]
+    btn3.style.backgroundColor = 'blue'
+}
+
+function focusEvent(){
+    let btn4 = document.getElementsByTagName('button')[4]
+    btn4.style.backgroundColor = 'blue'
+}
+
+function blurEvent(){
+    let btn5 = document.getElementsByTagName('button')[5]
+    btn5.style.backgroundColor = 'blue'
+}
--- dom03/data.js
+++ dom03/data.js
@@ -1,7 +1,4 @@
 function changeColor(){
     let p_tag_ref = document.getElementsByTagName('button')[0]
-
-console.log(p_tag_ref)
-
-p_tag_ref.style.backgroundColor = 'blue'
+    p_tag_ref.style.backgroundColor = 'blue'
 }
