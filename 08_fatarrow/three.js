3b3e6dd1151a4b7ecd95e6a5af924dc884e83dac
abhayonkar <abhayronkar@gmail.com>
2024-06-17 23:33:21 +0530

    fatarrow

---
+++ 08_fatarrow/three.js
@@ -0,0 +1,5 @@
+const gfg = ( x, y, z = 30 ) => {
+    console.log( x + " " + y + " " + z);
+}
+
+gfg( 10, 20 );
---
+++ 08_fatarrow/two.js
@@ -0,0 +1,5 @@
+const gfg = ( x, y, z ) => {
+    console.log( x + y + z )
+}
+
+gfg( 10, 20, 30 );
